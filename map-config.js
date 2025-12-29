4a3495845747f7625ea6877240a3c8e97c7d736c
Luis Vieira <38043622+luisjfvieira@users.noreply.github.com>
2025-12-29 20:59:31 +0000

    Add satellite layers and update initial state

--- map-config.js
+++ map-config.js
@@ -1,21 +1,28 @@
 const MAP_SOURCES = {
-    // Esri World Imagery (High-res Satellite)
-    satellite: {
-        type: 'raster',
-        tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-        tileSize: 256,
-        attribution: 'Tiles &copy; Esri'
-    },
-    // OpenStreetMap Standard (Vector/Street)
     vector: {
         type: 'raster', 
         tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
         tileSize: 256,
         attribution: '&copy; OpenStreetMap'
+    },
+    satellite_now: {
+        type: 'raster',
+        tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
+        tileSize: 256,
+        maxzoom: 22, // Enable high resolution
+        attribution: 'Esri'
+    },
+    satellite_2014: {
+        type: 'raster',
+        tiles: ['https://wayback.arcgisonline.com/arcgis/rest/services/World_Imagery_2014_02_20/MapServer/tile/{z}/{y}/{x}'],
+        tileSize: 256,
+        maxzoom: 22,
+        attribution: 'Esri Wayback'
     }
 };
 
 const INITIAL_STATE = {
     center: [-9.135, 38.725], // Arroios, Lisbon
-    zoom: 13
+    zoom: 13,
+    maxZoom: 22 
 };
