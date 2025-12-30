08a8184a7f01bda19693a4fc5d34d1a2a1bd3583
Luis Vieira <38043622+luisjfvieira@users.noreply.github.com>
2025-12-30 01:00:04 +0000

    Update map-config.js

--- map-config.js
+++ map-config.js
@@ -1,26 +1,11 @@
 const MAP_SOURCES = {
-    vector: {
-        type: 'raster',
-        tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
-        tileSize: 256,
-        maxzoom: 19
-    },
-    satellite_esri: {
-        type: 'raster',
-        tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-        tileSize: 256,
-        maxzoom: 22
-    },
-    satellite_google: {
-        type: 'raster',
-        tiles: ['https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'],
-        tileSize: 256,
-        maxzoom: 22
-    }
+    vector: { type: 'raster', tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'], tileSize: 256 },
+    satellite_esri: { type: 'raster', tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'], tileSize: 256 },
+    satellite_google: { type: 'raster', tiles: ['https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'], tileSize: 256 }
 };
 
 const LAND_USE_COLORS = {
-    'unassigned': '#555555', // Solid Dark Grey
+    'unassigned': '#666666',
     'cars': '#e74c3c',
     'pedestrians': '#2ecc71',
     'cycling': '#3498db',
@@ -29,6 +14,5 @@ const LAND_USE_COLORS = {
 
 const INITIAL_STATE = {
     center: [-9.135, 38.725],
-    zoom: 15,
-    maxZoom: 22
+    zoom: 14
 };
